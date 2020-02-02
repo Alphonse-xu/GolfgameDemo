e34e63995129259f452b6fbf331ef92c8bf280ae
Alphonse-xu <bluerailgun@outlook.com>
2020-02-02 23:35:55 +0000

    unityGameDemo

---
+++ 2DGolfGame/Assets/Scripts/movement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    // private Animator anim;
+
+    [Header("移动参数")]
+    public float speed = 8f;
+    public float jumpForce = 6.3f;
+
+    private float horizontalMove;
+   // public Transform groundCheck;
+   public LayerMask groundLayer;
+   public bool isGround; //是否落地
+
+    //按键设置
+   bool jumpPressed;
+   // int jumpCount;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       // coll = GetComponent<Collider2D>();
+       // anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        //{
+        //    jumpPressed = true;
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        //isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
+
+        GroundMovement();
+
+       // Jump();
+
+
+      //  SwitchAnim();
+    }
+
+    void GroundMovement()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");//只返回-1，0，1
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+        /*
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+        */
+    }
+    /*
+    void Jump()//跳跃
+    {
+        if (isGround)
+        {
+            jumpCount = 2;//可跳跃数量
+            isJump = false;
+        }
+        if (jumpPressed && isGround)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+
+    void SwitchAnim()//动画切换
+    {
+        anim.SetFloat("running", Mathf.Abs(rb.velocity.x));
+
+        if (isGround)
+        {
+            anim.SetBool("falling", false);
+        }
+        else if (!isGround && rb.velocity.y > 0)
+        {
+            anim.SetBool("jumping", true);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            anim.SetBool("jumping", false);
+            anim.SetBool("falling", true);
+        }
+    }
+    */
+}
