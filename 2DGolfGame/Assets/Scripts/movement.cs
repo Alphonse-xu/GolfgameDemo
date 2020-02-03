9782a688bc140894a2eb02efdb71257acf26ed0b
Alphonse-xu.github.io <bluerailgun@outlook.com>
2020-02-03 15:12:21 +0000

    move

--- 2DGolfGame/Assets/Scripts/movement.cs
+++ 2DGolfGame/Assets/Scripts/movement.cs
@@ -7,7 +7,7 @@ public class movement : MonoBehaviour
     // Start is called before the first frame update
 
     private Rigidbody2D rb;
-    private Collider2D coll;
+    private BoxCollider2D coll;
     // private Animator anim;
 
     [Header("移动参数")]
@@ -15,9 +15,21 @@ public class movement : MonoBehaviour
     public float jumpForce = 6.3f;
 
     private float horizontalMove;
-   // public Transform groundCheck;
+    // public Transform groundCheck;
+
+    [Header("状态")]
+    public bool isJump;
+    public bool isOnGround; //是否落地
+
+    [Header("环境监测")]
    public LayerMask groundLayer;
-   public bool isGround; //是否落地
+
+    //碰撞体尺寸
+    Vector2 colliderStandSize;
+    Vector2 colliderStandOffset;
+    Vector2 colliderBigSize;
+    Vector2 colliderBigOffset;
+
 
     //按键设置
    bool jumpPressed;
@@ -26,42 +38,69 @@ public class movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       // coll = GetComponent<Collider2D>();
+        coll = GetComponent<BoxCollider2D>();
+
+        colliderStandSize = coll.size;
+        colliderStandOffset = coll.offset;
+        colliderBigSize = new Vector2(coll.size.x * 2, coll.size.y * 2);
+        colliderBigOffset = new Vector2(coll.size.x * 2, coll.size.y * 2);
        // anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetButtonDown("Jump") && jumpCount > 0)
-        //{
-        //    jumpPressed = true;
-        //}
+        jumpPressed = Input.GetButton("Jump");
+            
     }
 
     private void FixedUpdate()
     {
-        //isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
-
+        PhysicsCheck();
         GroundMovement();
+        MidAirMovement();
 
        // Jump();
+        //  SwitchAnim();
+    } 
 
 
-      //  SwitchAnim();
+    void PhysicsCheck()
+    {
+        if(coll.IsTouchingLayers(groundLayer))
+        {
+            isOnGround = true;
+            isJump = false;
+        }
+        else
+        {
+            isOnGround = false;
+        }
     }
 
     void GroundMovement()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");//只返回-1，0，1
         rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
-        /*
+        
+        //人物反转
         if (horizontalMove != 0)
         {
-            transform.localScale = new Vector3(horizontalMove, 1, 1);
+            transform.localScale = new Vector3(horizontalMove * 1.4f, 1.4f, 1.4f);
         }
-        */
+        
     }
+
+    void MidAirMovement()
+    {
+        if (jumpPressed && isOnGround && !isJump)
+        {
+            isOnGround = false;
+            isJump = true;
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
     /*
     void Jump()//跳跃
     {
